3029156beaae7cb02166741232c0295ea761b9f6
Cristian Lucian IOAN <cristian-ioan@users.noreply.github.com>
2018-11-11 16:04:36 +0200

    firt commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,11 @@
+public class Client {
+
+    private int idClient;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private int phone;
+    private String bankAccount;
+
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+
+    private int idCategory;
+    private int idProduct;
+    private String categoryName;
+    private String productName;
+    private int priceProduct;
+    private int stockProduct;
+
+}
