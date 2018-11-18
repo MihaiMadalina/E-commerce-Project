46a3b17e55663101a7c338c950bcdbd41a225af9
SorinaBoz <34556456+SorinaBoz@users.noreply.github.com>
2018-11-18 10:53:58 +0200

    Add files via upload

--- src/main/java/client/ClientConsoleWriter.java
+++ src/main/java/client/ClientConsoleWriter.java
@@ -1,9 +1,26 @@
 package client;
 
+import java.util.Scanner;
+
 public class ClientConsoleWriter {
-    Client client = new Client();
-    public void displayUserData(){
-        System.out.println("Datele tale sunt " + client.getFirstName() + " " + client.getLastName() + " " +
+
+    public static void displayClientData(){
+        Client client = new Client();
+        System.out.println("Datele tale clientului " + client.getFirstName() + " " + client.getLastName() + " " +
                 client.getAddress() + " " + client.getEmail() + " " + client.getPhone());
+    }
+
+    public static void displayMenuClients(){
+        System.out.println("1. Display client menu");
+        System.out.println("2. Add client");
+        System.out.println("3.Edit client");
+        System.out.println("4.Delete client");
+        System.out.println("0 - Exit");
+        System.out.println("Select your option");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        switch (option){
+            case 1: 
+        }
     }
 }
